794d2c74bf00c0fd529069d001c5b81c9178bbc0
Marcos <bacelomarcos@gmail.com>
2024-08-25 17:28:21 -0300

    Implemented cache-first strategy

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,36 +1,54 @@
-const cacheName = 'guitar-jukebox-site-v1.0'
+const CACHE_NAME = 'guitar-jukebox-site-v1.0'
 const resourcesToPrecache = [
   'index.html',
   'css/style.css',
-  'js/app-utils.js',
+  'js/api-utils.js',
   'js/dom-utils.js',
   'js/event-handlers.js'
 ]
 
 self.addEventListener('install', installEvent => {
   installEvent.waitUntil(
-    caches.open(cacheName).then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       cache.addAll(resourcesToPrecache)
     })
   )
 })
 
-//The stale-while-revalidate strategy serves the cached resource immediately while simultaneously fetching the latest version from the network. 
-//The cache is updated with the new response. This approach ensures that users get fast responses and that the cache stays up to date.
+// Fetch event - cache-first strategy
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {      
-      // Start a network request in the background
-      const networkFetch = fetch(event.request).then(networkResponse => {
-        // Update the cache with the new response
-        return caches.open(cacheName).then(cache => {
+    caches.match(event.request).then(cachedResponse => {
+      // Return the cached response if found
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // Otherwise, fetch from the network
+      return fetch(event.request).then(networkResponse => {
+        // Cache the new response for future requests
+        return caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, networkResponse.clone());
           return networkResponse;
         });
       });
+    })
+  );
+});
 
-      // Return the cached response immediately, or wait for network if not cached
-      return cachedResponse || networkFetch;
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME]; // Only the current cache should be kept
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // If the cache name is not in the whitelist, delete it
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
